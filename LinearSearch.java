aa96ae9e922a365841105dfe0f46e5e789aff656
Muid Hasan Maruf <121661366+MUID-MARUF@users.noreply.github.com>
2023-12-26 23:48:35 +0600

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class LinearSearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5]; 
+        int target; 
+        int location = 0;
+        boolean checker = false;
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        target = sc.nextInt();
+
+        for (int i = 0; i < 5; i++) {
+            if (arr[i] == target) {
+                location = i + 1;
+                checker = true;
+                break;
+            }
+        }
+
+        if (checker == true) {
+            System.out.println(location + " is the position of the data.");
+        }
+
+        else {
+            System.out.println("Data not found.");
+        }
+    }
+}
