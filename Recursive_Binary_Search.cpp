b63d4fe31178eca18f723a20864ee11e748b1038
Muid Hasan Maruf <121661366+MUID-MARUF@users.noreply.github.com>
2024-01-30 21:49:15 +0600

    Update Recursive_Binary_Search.cpp

--- Recursive_Binary_Search.cpp
+++ Recursive_Binary_Search.cpp
@@ -1,46 +1,72 @@
 #include <iostream>
 using namespace std;
 
+//Must input numbers in accending order.
+
 int BinarySearch(int list[], int low, int high, int key)
 {
-    int mid=(high + low)/2;
+    int mid = (high + low) / 2;
 
-    if(low<=high)
+    if (low <= high)
     {
         if (list[mid] == key)
         {
-        return mid;
+            return mid;
         }
-        else if (list[mid] >key)
-            return BinarySearch(list,low,mid -1, key);
+        else if (list[mid] > key)
+            return BinarySearch(list, low, mid - 1, key);
         else
-            return BinarySearch(list,high,mid + 1, key);
+            return BinarySearch(list, high, mid + 1, key);
     }
-    else return -1;
+    else
+        return -1;
+}
+
+void print_arr(int arr[], int arr_size)
+{
+    cout << "Your array is :: " << endl;
+    for (int i = 0; i < arr_size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void input_arr(int arr[], int arr_size)
+{
+    cout << "What are your " << arr_size << " elements of the array ?" << endl;
+    for (int i = 0; i < arr_size; i++)
+    {
+        cin >> arr[i];
+    }
 }
 
 int main()
 {
-    int Array[] = {1, 3, 4, 6, 9, 10, 14, 19, 23, 30};
-    int first = 0;
-    int last = 9;
-    int value;
+    int a, arr_size;
 
-    for( int i= 0; i< 10; i++)
-    {
-        cout << "{" <<Array[i] <<"} ";
-    }
+    cout << "What is your array size ?" << endl;
+    cin >> a;
+    int arr[a];
+    arr_size = sizeof(arr) / sizeof(arr[0]);
 
-    cout << "\n\nPlease enter a number to find from the above list of number. \n";
-    cin >> value;
-    cout << "\n\n";
+    input_arr(arr, arr_size);
 
-    int Location = BinarySearch(Array, first, last, value);
+    print_arr(arr, arr_size);
+
+    cout << "Which number you want to find ?" << endl;
+    int key;
+
+    cin >> key;
+
+    int l = 0;
+    int r = arr_size - 1;
+
+    int Location = BinarySearch(arr, l, r, key);
 
     if(Location==-1)
-         cout<< "Number is not found.\n";
+         cout<< "Number is NOT in the array.\n"<< endl;
     else
-         cout<< "Number is found.\n";
+         cout<< "Number is in the array."<< endl;
     return 0;
 }
-
